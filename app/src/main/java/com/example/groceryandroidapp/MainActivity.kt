da544016e5c3c0f1f1a1e58bc33a7812b000c59c
Chetan Singh Negi <chetansinghnegicse@gmail.com>
2022-09-17 13:11:41 +0530

    Create RecyclerView & FloatingActionButton

--- app/src/main/java/com/example/groceryandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/groceryandroidapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.groceryandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var itemsRV :RecyclerView
+    lateinit var addFAB: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        itemsRV = findViewById(R.id.idRVItems)
+        addFAB = findViewById(R.id.idFABAdd)
+
     }
 }
